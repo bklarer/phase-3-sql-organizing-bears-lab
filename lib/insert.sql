50f596554ed1220202967d012c504c681c74dcc4
Benjamin Klarer <benjaminklarer@gmail.com>
2022-10-21 22:35:58 -0700

    commit tree 36cd7d9e8cd1d5ac205982d1892528098e0b7115

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'black', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'F', 'white', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'M', 'black', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M', 'black', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 11, 'F', 'white', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 13, 'M', 'black', 'calm', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 15, 'F', 'white', 'calm', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'black', 'calm', 1);
